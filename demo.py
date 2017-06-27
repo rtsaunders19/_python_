bf115e0c3cd10dd6b901d1a2158b5bc8916ffa80
Robert Saunders <rtsaunders19@gmail.com>
2017-06-26 21:53:42 -0500

    commit

---
+++ demo.py
@@ -0,0 +1,21 @@
+names = ['apple']
+
+print(len(names))
+
+def fruits():
+    print('apple')
+    print('oranges')
+    print('pears')
+
+fruits()
+
+boots = ['apple', 'cheese', 'cake', 'pear']
+
+for x in range(1,6):
+    print("Rob")
+
+for x in boots:
+    print(x)
+
+for x in range(0,21,2):
+    print(x)
---
+++ statement.py
@@ -0,0 +1,10 @@
+marks = int(input('enter your marks'))
+
+if marks >= 90:
+    print('A')
+elif marks >= 70:
+    print('B')
+elif marks >= 60:
+    print('C')
+elif marks < 60:
+    print('D')
